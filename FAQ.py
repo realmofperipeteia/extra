14d93db46ae5dbe00e1f39832784cd9b388d268e
DarkPrinceNeo <41468228+DarkPrinceNeo@users.noreply.github.com>
2020-08-01 01:50:14 +1200

    Create FAQ.py

---
+++ FAQ.py
@@ -0,0 +1,27 @@
+!servalias FAQ embed
+<drac2>
+args,n=&ARGS&,"\n"
+
+Msg=f""" -desc 
+	"
+	:one: **Is UA allowed?**
+	No.
+	
+	:two: **What sources are approved?**
+	Please read the guidelines
+	
+	:three: **Where do I link my character?**
+	#📝│submit 
+	
+	:four: **Where can I buy/sell items?**
+	#🎲│transactions 
+	Items are sold at half value
+	
+	"
+	"""
+return Msg
+</drac2>
+-title "Frequently Asked Questions"
+-footer "Help | FAQ | Peripéteia"
+-thumb <image>
+-color <color>
